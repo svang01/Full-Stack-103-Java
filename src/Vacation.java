0320fc4fb3f0b2fb5b4311955b7ef0fee2540e5c
svang01 <127761892+svang01@users.noreply.github.com>
2023-05-08 21:57:04 -0400

    Add files via upload

---
+++ src/Vacation.java
@@ -0,0 +1,8 @@
+public class Vacation {
+    public static void main(String[] args) {
+        System.out.println("I'd Like to Visit");
+        System.out.println("Tokyo");
+        System.out.println("Japan");
+    }
+}
+
